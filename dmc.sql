baa10168a69e698bf103029d76619b8cbe3162ce
srikeerthis <srikeerthisrinivasan@gmail.com>
2019-12-22 13:21:49 +0530

    made minor change in Layer table

--- dmc.sql
+++ dmc.sql
@@ -5,7 +5,8 @@ create table Employee(EmpID varchar(20),Fname varchar(20) not null,Lname varchar
 create table Organisation(Name varchar(20) not null,FuncID varchar(20),TeamID varchar(20),EmpID varchar(20),foreign key(EmpID) references Employee(EmpID),primary key(FuncID,TeamID));
 
 --Layer table
-create table Layer(EmpID varchar(20),LayerID varchar(20) not null,Dim1 int(1) default null,Dim2 int(1) default null,Dim3 int(1) default null,Dim4 int(1) default null,Dim5 int(1) default null,Dim6 int(1) default null,Dim7 int(1) default null,Dim8 int(1) default null,Dim9 int(1) default null,LayerAvg double(5,3),foreign key(EmpID) references Employee(EmpID));
+create table Layer(EmpID varchar(20),LayerID varchar(20) not null,Dim1 int default 0,Dim2 int default 0,Dim3 int default 0,Dim4 int default 0,Dim5 int default 0,Dim6 int default 0,Dim7 int default 0,Dim8 int default 0,Dim9 int default 0,LayerAvg double(5,3),foreign key(EmpID) references Employee(EmpID));
 
 --Score table
 create table Score(ScoreID int(5),EmpID varchar(20),foreign key(EmpID) references Employee(EmpID),Average double(5,3),primary key(ScoreID));
+
